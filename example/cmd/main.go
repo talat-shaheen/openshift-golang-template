177373ab05a13591e562d205fb0b58dedf30efac
talat-shaheen <36874355+talat-shaheen@users.noreply.github.com>
2019-11-14 13:52:05 +0530

    Update main.go

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -4,6 +4,9 @@ import (
 "io";
 "net/http")
 
+func writeRootData(w http.ResponseWriter, r *http.Request){
+io.WriteString(w, "hi there!\n")
+}
 func writeData(w http.ResponseWriter, r *http.Request){
 io.WriteString(w, "hello\n")
 }
@@ -12,8 +15,9 @@ w.Write([]byte("pong\n"))
 }
 
 func main(){
-fmt.Println("1111111111111111")
-http.Handle("/",http.FileServer(http.Dir("/root/www")))
+//fmt.Println("1111111111111111")
+//http.Handle("/",http.FileServer(http.Dir("/root/www")))
+http.HandleFunc("/",writeRootData)
 http.HandleFunc("/hello",writeData)
 http.HandleFunc("/ping",ping)
 http.ListenAndServe(":8000",nil)
