1b13a95e07d5202a52b9439eeb5d3ebe96b9aaf6
talat-shaheen <36874355+talat-shaheen@users.noreply.github.com>
2019-11-19 11:07:42 +0530

    Update main.go

--- example/cmd/main.go
+++ example/cmd/main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 	"github.com/gorilla/mux"
 	"html/template"
+	"database/sql"
+	"log"
+        "time"
+        _ "github.com/go-sql-driver/mysql"
 )
 type ContactDetails struct {
     Email   string
@@ -32,6 +36,100 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 //}
 
 func main() {
+	//db related code
+	db, err := sql.Open(dbname, user:password@(ip:port)/user?parseTime=true")
+    if err != nil {
+        log.Fatal(err)
+    }
+    if err := db.Ping(); err != nil {
+        log.Fatal(err)
+    }
+
+    { // Create a new table
+        query := `
+            CREATE TABLE users (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+        if _, err := db.Exec(query); err != nil {
+            log.Fatal(err)
+        }
+    }
+
+    { // Insert a new user
+        username := "johndoe"
+        password := "secret"
+        createdAt := time.Now()
+
+        result, err := db.Exec(`INSERT INTO users (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        id, err := result.LastInsertId()
+        fmt.Println(id)
+    }
+
+    { // Query a single user
+        var (
+            id        int
+            username  string
+            password  string
+            createdAt time.Time
+        )
+
+        query := "SELECT id, username, password, created_at FROM users WHERE id = ?"
+        if err := db.QueryRow(query, 1).Scan(&id, &username, &password, &createdAt); err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Println(id, username, password, createdAt)
+    }
+
+    { // Query all users
+        type user struct {
+            id        int
+            username  string
+            password  string
+            createdAt time.Time
+        }
+
+        rows, err := db.Query(`SELECT id, username, password, created_at FROM users`)
+        if err != nil {
+            log.Fatal(err)
+        }
+        defer rows.Close()
+
+        var users []user
+        for rows.Next() {
+            var u user
+
+            err := rows.Scan(&u.id, &u.username, &u.password, &u.createdAt)
+            if err != nil {
+                log.Fatal(err)
+            }
+            users = append(users, u)
+        }
+        if err := rows.Err(); err != nil {
+            log.Fatal(err)
+        }
+
+        fmt.Printf("%#v", users)
+    }
+
+    {
+        _, err := db.Exec(`DELETE FROM users WHERE id = ?`, 1)
+        if err != nil {
+            log.Fatal(err)
+        }
+    }
+	
+	
+	
 	tmpl := template.Must(template.ParseFiles("/opt/app-root/src/github.com/talat-shaheen/openshift-golang-template/example/cmd/forms.html"))
 	r := mux.NewRouter()
 	//http.HandleFunc("/", helloHandler)
@@ -70,6 +168,7 @@ func main() {
 
         tmpl.Execute(w, struct{ Success bool }{true})
     })
+	
       http.ListenAndServe(":8080", r)
 	//go listenAndServe(port)
 
